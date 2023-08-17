6a09f7707cbb7e1028d10237f128cedfec5fd19d
Gianpierre Fernandez <fgianpierre@gmail.com>
2023-08-17 16:01:02 +0200

    update production database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,8 +4,8 @@ module.exports = ({env}) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5931),
+      host: env('PGHOST', 'localhost'),
+      port: env.int('PGPORT', 6684),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
       password: env('PGPASSWORD', 'password'),
