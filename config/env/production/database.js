a63c19c3228a230945f66ce561a43505d37a0e46
Gianpierre Fernandez <fgianpierre@gmail.com>
2023-08-17 16:31:47 +0200

    hide HOST and PORT variables in production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,8 +4,8 @@ module.exports = ({env}) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', 'localhost'),
-      port: env.int('PGPORT', 6684),
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5931),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
       password: env('PGPASSWORD', 'password'),
